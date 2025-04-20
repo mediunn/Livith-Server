6a60a431b709ed3c05cecd49d874a7d58792d1f9
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-20 17:28:47 +0900

    :sparkles: feat: 콘서트 목록 조회 API 구현

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 // import { OpenApiService } from './open-api/open-api.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
 
   //직접 호출
