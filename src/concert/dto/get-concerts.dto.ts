565d1d5f90c54b85c72fcd04e5cbcd8a64420a4c
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-20 18:54:04 +0900

    :memo: docs: 콘서트 목록 조회 API 스웨거 문서화

--- src/concert/dto/get-concerts.dto.ts
+++ src/concert/dto/get-concerts.dto.ts
@@ -1,16 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, IsNumber, IsOptional } from 'class-validator';
 import { ConcertStatus } from 'src/common/enums/concert-status.enum';
 
 export class GetConcertsDto {
+  @ApiProperty({
+    description: '콘서트 진행 상태',
+    default: 'ONGOING',
+    enum: ConcertStatus,
+    enumName: 'ConcertStatus',
+    example: 'ONGOING',
+    required: true,
+  })
   @IsEnum(ConcertStatus, {
     message: 'status는 ONGOING | UPCOMING | COMPLETED 중 하나여야 해요',
   })
   status: ConcertStatus;
 
+  @ApiProperty({
+    description: '커서(마지막 콘서트의 ID)',
+    default: 0,
+    required: false,
+    example: 0,
+  })
   @IsOptional()
   @IsNumber()
   cursor?: number;
 
+  @ApiProperty({
+    description: '가져올 데이터 개수',
+    default: 10,
+    required: false,
+    example: 10,
+  })
   @IsOptional()
   @IsNumber()
   size?: number;
