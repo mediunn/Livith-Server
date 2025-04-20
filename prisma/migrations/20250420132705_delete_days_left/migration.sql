a1ef78383a40a5322c2b922d9763fd458485ed68
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-20 22:33:15 +0900

    :bug: fix: 공연 남은 날짜 동적 계산으로 변경

---
+++ prisma/migrations/20250420132705_delete_days_left/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `days_left` on the `concerts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `concerts` DROP COLUMN `days_left`;
