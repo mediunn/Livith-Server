abe6b9b9a21c4a568bae2e8ee2bf4bbc621a6340
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-22 01:05:15 +0900

    :sparkles: feat: 스케쥴러로 매일 DB 정렬하도록 수정

--- src/concert/concert.module.ts
+++ src/concert/concert.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConcertController } from './concert.controller';
 import { ConcertService } from './concert.service';
 import { PrismaService } from 'prisma/prisma.service';
+import { ConcertSchedulerService } from './concert-scheduler.service';
 
 @Module({
   controllers: [ConcertController],
-  providers: [ConcertService, PrismaService],
+  providers: [ConcertService, PrismaService, ConcertSchedulerService],
 })
 export class ConcertModule {}
