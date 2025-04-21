ba31e51818f64a06496d44e8a3dc2f539e9f510e
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-21 17:35:25 +0900

    :bug: fix: 대소문자 구분 없이 필터링 하도록 수정

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -7,30 +7,22 @@ export class SearchService {
   async getRecommendWords(letter: string) {
     const recommendWords = await this.prismaService.concert.findMany({
       where: {
-        OR: [
-          {
-            title: {
-              contains: letter,
-            },
-          },
-          {
-            artist: {
-              contains: letter,
-            },
-          },
-        ],
+        OR: [{ title: { contains: letter } }, { artist: { contains: letter } }],
       },
-      take: 5, // 결과 갯수 제한
+      take: 5,
     });
 
     // title이나 artist에 포함된 값만 필터링
     const filteredResults = recommendWords.map((concert) => {
+      const lowerLetter = letter.toLowerCase();
+
       // title에 포함된 경우
-      if (concert.title.includes(letter)) {
+      if (concert.title.toLowerCase().includes(lowerLetter)) {
         return concert.title;
       }
+
       // artist에 포함된 경우
-      if (concert.artist.includes(letter)) {
+      if (concert.artist.toLowerCase().includes(lowerLetter)) {
         return concert.artist;
       }
     });
