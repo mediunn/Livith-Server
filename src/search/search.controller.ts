47fa59f22280aa2903ce04804df1a4e0e107b07f
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-22 21:15:43 +0900

    :memo: docs: 콘서트 검색 결과 조회 API 스웨거 문서화

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -8,6 +8,7 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { GetSearchResultsDto } from './dto/get-search-results.dto';
+import { ConcertResponseDto } from 'src/concert/dto/concert-response.dto';
 
 @ApiTags('검색')
 @Controller('search')
@@ -55,6 +56,17 @@ export class SearchController {
     return this.searchService.getRecommendWords(letter);
   }
   @Get()
+  @ApiOperation({
+    summary: '검색 결과 조회',
+    description: '검색어에 대한 결과를 조회합니다',
+  })
+  @ApiOkResponse({
+    description: '검색 결과 조회 성공',
+    type: ConcertResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: '잘못된 요청입니다.',
+  })
   getSearchResults(@Query() query: GetSearchResultsDto) {
     if (!query.keyword?.trim()) {
       throw new BadRequestException('검색어(keyword)는 필수입니다.');
