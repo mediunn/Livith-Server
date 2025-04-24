e424b0a2af8af628376dd0c4cdd1b4bcb0f4d50a
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-24 10:39:55 +0900

    :memo: docs: 특정 콘서트의 셋리스트 목록 조회 스웨거 문서화

--- src/setlist/dto/setlist-response.dto.ts
+++ src/setlist/dto/setlist-response.dto.ts
@@ -1,13 +1,40 @@
-import { Setlist, SetlistType } from 'generated/prisma';
+import { ApiProperty } from '@nestjs/swagger';
+import { Setlist, SetlistType } from '@prisma/client';
 
 export class SetlistResponseDto {
+  @ApiProperty({ example: 1, description: '셋리스트 ID' })
   id: number;
+  @ApiProperty({ example: 1, description: '콘서트 ID' })
   concertId: number;
+  @ApiProperty({
+    example: '2017 콜드플레이 월드투어',
+    description: '셋리스트 제목',
+  })
   title: string;
+  @ApiProperty({
+    example: 'ONGOING',
+    description: '셋리스트 타입',
+  })
   type: SetlistType;
+  @ApiProperty({
+    example: '1회차',
+    description: '셋리스트 상태',
+  })
   status: string;
+  @ApiProperty({
+    example: '2023-10-01',
+    description: '셋리스트 날짜',
+  })
   date: string;
+  @ApiProperty({
+    example: 'https://example.com/setlist1.jpg',
+    description: '셋리스트 이미지 URL',
+  })
   imgUrl: string;
+  @ApiProperty({
+    example: 'Coldplay',
+    description: '셋리스트 아티스트',
+  })
   artist: string;
 
   constructor(setlist: Setlist) {
