b695a94ecab7a3f1f13e5ecc974b0b346985ae42
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-04-26 22:33:05 +0900

    :memo: docs: 특정 노래의 가사 및 기타 정보 조회 API 스웨거 문서화

--- src/lyrics/dto/lyrics-response.dto.ts
+++ src/lyrics/dto/lyrics-response.dto.ts
@@ -1,11 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Song } from '@prisma/client';
+
 export class LyricsResponseDto {
+  @ApiProperty({
+    description: '노래 ID',
+    example: 1,
+  })
   id: number;
+  @ApiProperty({
+    description: '노래 제목',
+    example: 'Cruel Summer',
+  })
   title: string;
+  @ApiProperty({
+    description: '아티스트 이름',
+    example: 'Tayler Swift',
+  })
   artist: string;
+  @ApiProperty({
+    description: '가사 내용',
+    example: ['Cruel summer', 'it’s cool summer'],
+  })
   lyrics: string[];
+  @ApiProperty({
+    description: '발음 내용',
+    example: ['크루얼 썸머', '잇스 쿨 썸머'],
+  })
   pronunciation: string[];
+  @ApiProperty({
+    description: '번역 내용, 각 줄마다 배열로 구분',
+    example: ['잔인한 여름', '시원한 여름'],
+  })
   translation: string[];
-  constructor(song: any) {
+  constructor(song: Song) {
     this.id = song.id;
     this.title = song.title;
     this.artist = song.artist;
