42d7d6ec43fffcc6a8fcecc235bdeff0d8a413b4
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-05-19 00:09:17 +0900

    :sparkles: feat: 노래 csv 파일을 db에 저장하는 로직 추가

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -3,6 +3,7 @@ import { CultureUploadService } from './culture-upload.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { SetlistUploadService } from './setlist-upload.service';
 import { ConcertSetlistUploadService } from './concert-setlist-upload.service';
+import { SongUploadService } from './song-upload.service';
 
 @Module({
   imports: [PrismaModule],
@@ -10,11 +11,13 @@ import { ConcertSetlistUploadService } from './concert-setlist-upload.service';
     CultureUploadService,
     SetlistUploadService,
     ConcertSetlistUploadService,
+    SongUploadService,
   ],
   exports: [
     CultureUploadService,
     SetlistUploadService,
     ConcertSetlistUploadService,
+    SongUploadService,
   ],
 })
 export class UploadModule {}
