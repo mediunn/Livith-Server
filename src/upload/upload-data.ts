20a3796022c3f7b14d16871270716ff840e4665d
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-05-18 23:28:50 +0900

    콘서트 셋리스트 csv 파일을 db에 저장하는 로직 추가

--- src/upload/upload-data.ts
+++ src/upload/upload-data.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
 import { CultureUploadService } from './culture-upload.service';
 import { SetlistUploadService } from './setlist-upload.service';
+import { ConcertSetlistUploadService } from './concert-setlist-upload.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const cultureService = app.get(CultureUploadService);
   const setlistService = app.get(SetlistUploadService);
+  const concertSetlistService = app.get(ConcertSetlistUploadService);
 
   await cultureService.uploadCulturesFromCSV('csv/cultures.csv');
   await setlistService.uploadSetlistsFromCSV('csv/setlists.csv');
+  await concertSetlistService.uploadConcertSetlistsFromCSV(
+    'csv/concert_setlists.csv',
+  );
 
   await app.close();
 }
--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { CultureUploadService } from './culture-upload.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { SetlistUploadService } from './setlist-upload.service';
+import { ConcertSetlistUploadService } from './concert-setlist-upload.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [CultureUploadService, SetlistUploadService],
-  exports: [CultureUploadService, SetlistUploadService],
+  providers: [
+    CultureUploadService,
+    SetlistUploadService,
+    ConcertSetlistUploadService,
+  ],
+  exports: [
+    CultureUploadService,
+    SetlistUploadService,
+    ConcertSetlistUploadService,
+  ],
 })
 export class UploadModule {}
