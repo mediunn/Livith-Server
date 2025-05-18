153f1505a168928f69bbc7c80031d6f16130f725
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-05-18 23:12:25 +0900

    :sparkles: feat: 셋리스트 csv 파일을 db에 저장하는 로직 추가

--- src/upload/upload-data.ts
+++ src/upload/upload-data.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
 import { CultureUploadService } from './culture-upload.service';
+import { SetlistUploadService } from './setlist-upload.service';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   const cultureService = app.get(CultureUploadService);
+  const setlistService = app.get(SetlistUploadService);
 
   await cultureService.uploadCulturesFromCSV('csv/cultures.csv');
+  await setlistService.uploadSetlistsFromCSV('csv/setlists.csv');
 
   await app.close();
 }
