f3d28c132bdc7e00b358af1331689c37f5d776f2
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-05-18 23:12:25 +0900

    :sparkles: feat: 셋리스트 csv 파일을 db에 저장하는 로직 추가

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CultureUploadService } from './culture-upload.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { SetlistUploadService } from './setlist-upload.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [CultureUploadService],
-  exports: [CultureUploadService],
+  providers: [CultureUploadService, SetlistUploadService],
+  exports: [CultureUploadService, SetlistUploadService],
 })
 export class UploadModule {}
