4f1b49510da69df28579b7e86843385e3140dbe6
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-07-25 15:56:44 +0900

    :sparkles: feat: 콘서트 목록 조회 API v2 구현

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { AppModuleV1 } from './v1/app.module';
+import { AppModuleV2 } from './v2/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import { ConcertSchedulerService } from './concert/concert-scheduler.service';
 // import { OpenApiService } from './open-api/open-api.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModuleV2);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,7 +22,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Livith API 문서')
     .setDescription('Livith API 문서입니다.')
-    .setVersion('1.0')
+    .setVersion('2.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
