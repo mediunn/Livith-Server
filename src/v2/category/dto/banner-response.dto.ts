c44938251a003d514740b13f34cea0b3d48f54fa
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-08-06 13:01:05 +0900

    :sparkles: feat: 배너에 내용 칼럼 추가

--- src/v2/category/dto/banner-response.dto.ts
+++ src/v2/category/dto/banner-response.dto.ts
@@ -5,10 +5,12 @@ export class BannerResponseDto {
   title: string;
   category: string;
   imgUrl: string;
+  content: string;
   constructor(banner: Banner) {
     this.id = banner.id;
     this.title = banner.title;
     this.category = banner.category;
     this.imgUrl = banner.imgUrl;
+    this.content = banner.content;
   }
 }
