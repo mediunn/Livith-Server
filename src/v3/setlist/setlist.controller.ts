91c3b8c8a05cc1ed31b9ac0e5c5a3a16c6dcc710
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-08-25 15:38:49 +0900

    :sparkles: feat: 특정 셋리스트의 곡 목록 조회 api v3 구현

---
+++ src/v3/setlist/setlist.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { SetlistService } from './setlist.service';
+import { ParsePositiveIntPipe } from '../common/pipes/parse-positive-int.pipe';
+
+@ApiTags('셋리스트')
+@Controller('api/v3/setlists/')
+export class SetlistController {
+  constructor(private readonly setlistService: SetlistService) {}
+
+  //셋리스트 노래 목록 조회
+  @Get(':id/songs')
+  @ApiOperation({
+    summary: '특정 셋리스트의 곡 목록 조회',
+    description: '특정 셋리스트의 곡 목록을 조회합니다.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: '셋리스트의 ID',
+    type: Number,
+    example: 1,
+  })
+  getSetlistSongs(@Param('id', ParsePositiveIntPipe) id: number) {
+    return this.setlistService.getSetlistSongs(id);
+  }
+}
---
+++ src/v3/setlist/setlist.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { SongResponseDto } from './dto/song-response.dto';
+
+@Injectable()
+export class SetlistService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  //셋리스트 노래 목록 조회
+  async getSetlistSongs(id: number) {
+    // 셋리스트 ID가 유효한지 확인
+    const setlist = await this.prismaService.setlist.findUnique({
+      where: { id },
+    });
+    if (!setlist) {
+      throw new NotFoundException('해당 셋리스트가 존재하지 않습니다.');
+    }
+
+    // 셋리스트에 해당하는 곡 목록 조회
+    const setlistSongs = await this.prismaService.setlistSong.findMany({
+      where: { setlistId: id },
+      include: {
+        song: true, // 곡 정보 포함
+      },
+      orderBy: { orderIndex: 'asc' },
+    });
+
+    return setlistSongs.map(
+      (setlistSong) =>
+        new SongResponseDto(setlistSong.song, setlistSong.orderIndex),
+    );
+  }
+}
