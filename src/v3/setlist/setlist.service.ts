eb4209f7e10763d778f455f701b924e2e2565a85
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-08-25 15:49:58 +0900

    :sparkles: feat: 특정 노래 응원법, 응원법 포인트 조회 api 구현

--- src/v3/setlist/setlist.service.ts
+++ src/v3/setlist/setlist.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { SongResponseDto } from './dto/song-response.dto';
+import { FanchantResponseDto } from './dto/fanchant-response.dto';
 
 @Injectable()
 export class SetlistService {
@@ -29,5 +30,40 @@ export class SetlistService {
       (setlistSong) =>
         new SongResponseDto(setlistSong.song, setlistSong.orderIndex),
     );
+  }
+
+  // 곡 응원법 조회
+  async getSongFanchant(setlistId: number, songId: number) {
+    // 셋리스트 ID가 유효한지 확인
+    const setlist = await this.prismaService.setlist.findUnique({
+      where: { id: setlistId },
+    });
+    if (!setlist) {
+      throw new NotFoundException('해당 셋리스트가 존재하지 않습니다.');
+    }
+
+    // 곡 ID가 유효한지 확인
+    const song = await this.prismaService.song.findUnique({
+      where: { id: songId },
+    });
+    if (!song) {
+      throw new NotFoundException('해당 곡이 존재하지 않습니다.');
+    }
+
+    // 응원법 조회
+    const fanchant = await this.prismaService.setlistSong.findFirst({
+      where: {
+        setlistId,
+        songId,
+      },
+    });
+
+    if (!fanchant) {
+      throw new NotFoundException(
+        '해당 셋리스트와 곡의 조합이 존재하지 않습니다.',
+      );
+    }
+
+    return new FanchantResponseDto(fanchant);
   }
 }
