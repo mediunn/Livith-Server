75783a11e9f4a8c526284b739878d9ba4b8fee7b
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-08-26 01:23:45 +0900

    :recycle: refact: 콘서트 목록 조회 정렬 시 startDate 활용하도록 변경

--- src/v3/concert/dto/get-concerts.dto.ts
+++ src/v3/concert/dto/get-concerts.dto.ts
@@ -1,30 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNumber, IsOptional, Min } from 'class-validator';
-import { ConcertFilter } from 'src/v3/common/enums/concert-filter.enum';
+import { IsNumber, IsOptional, Min } from 'class-validator';
 export class GetConcertsDto {
   @ApiProperty({
-    description: '콘서트 진행 상태',
-    default: 'ALL',
-    enum: ConcertFilter,
-    enumName: 'ConcertFilter',
-    example: 'ALL',
-    required: true,
-  })
-  @IsEnum(ConcertFilter, {
-    message: 'filter는 NEW | UPCOMING | ALL 중 하나여야 해요',
-  })
-  filter: ConcertFilter;
-
-  @ApiProperty({
-    description: '커서 (id 또는 sortedIndex)',
+    description: '커서 (startDate)',
     default: undefined,
     required: false,
     example: undefined,
   })
   @IsOptional()
+  cursor?: string;
+
+  @ApiProperty({
+    description: '콘서트의 ID',
+    required: false,
+    example: 1,
+  })
+  @IsOptional()
   @IsNumber()
-  @Min(0)
-  cursor?: number;
+  @Min(1)
+  id?: number;
 
   @ApiProperty({
     description: '가져올 데이터 개수',
