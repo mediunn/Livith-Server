56a5d86995c2f03a9a5e1363e081ed002a6bb26c
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-08-28 01:51:00 +0900

    :sparkles: feat: 추천 검색어 조회 api v3 구현

--- src/v3/search/search.controller.ts
+++ src/v3/search/search.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { SearchService } from './search.service';
 
 @ApiTags('탐색')
@@ -14,5 +14,25 @@ export class SearchController {
   })
   getBanners() {
     return this.searchService.getBanners();
+  }
+
+  //추천 검색어 조회
+
+  @Get('/suggestions')
+  @ApiOperation({
+    summary: '추천 검색어 조회',
+    description: '추천 검색어 10개를 조회합니다',
+  })
+  @ApiQuery({
+    name: 'letter',
+    description: '검색할 글자',
+    type: String,
+    example: 'a',
+  })
+  getRecommendWords(@Query('letter') letter: string) {
+    if (!letter?.trim()) {
+      throw new BadRequestException('검색어(letter)는 필수입니다.');
+    }
+    return this.searchService.getRecommendWords(letter);
   }
 }
--- src/v3/search/search.service.ts
+++ src/v3/search/search.service.ts
@@ -13,4 +13,31 @@ export class SearchService {
     });
     return banners.map((banner) => new BannerResponseDto(banner));
   }
+
+  //추천 검색어 조회
+  async getRecommendWords(letter: string) {
+    const recommendWords = await this.prismaService.concert.findMany({
+      where: {
+        OR: [{ title: { contains: letter } }, { artist: { contains: letter } }],
+      },
+      take: 10,
+    });
+
+    // title이나 artist에 포함된 값만 필터링
+    const filteredResults = recommendWords.map((concert) => {
+      const lowerLetter = letter.toLowerCase();
+
+      // title에 포함된 경우
+      if (concert.title.toLowerCase().includes(lowerLetter)) {
+        return concert.title;
+      }
+
+      // artist에 포함된 경우
+      if (concert.artist.toLowerCase().includes(lowerLetter)) {
+        return concert.artist;
+      }
+    });
+
+    return filteredResults;
+  }
 }
