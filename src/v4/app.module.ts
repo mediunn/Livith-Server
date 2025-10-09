131d56b08b99738447bff3307aaf8f4c1c164ff0
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-10-09 17:43:40 +0900

    :sparkles: feat: 카카오 로그인 및 로그아웃 구현

--- src/v4/app.module.ts
+++ src/v4/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { GlobalResponseInterceptor } from './common/interceptors/global-response.interceptor';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [
