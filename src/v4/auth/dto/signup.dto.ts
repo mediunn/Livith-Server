b165312ec65d4c8d12315235cd7962059ae22306
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-10-19 22:13:04 +0900

    :recycle: refact: 새 유저일 경우 회원가입 시 토큰 발급하도록 수정

--- src/v4/auth/dto/signup.dto.ts
+++ src/v4/auth/dto/signup.dto.ts
@@ -1,7 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MaxLength } from 'class-validator';
+import { IsBoolean, IsNotEmpty, MaxLength } from 'class-validator';
 
 export class SignupDto {
+  @ApiProperty({
+    description: 'provider',
+    example: 'kakao',
+  })
+  @IsNotEmpty()
+  provider: string;
+
+  @ApiProperty({
+    description: 'provider ID',
+    example: 4480239560,
+  })
+  @IsNotEmpty()
+  providerId: string;
+
+  @ApiProperty({
+    description: '이메일',
+    example: 'livith@gmail.com',
+  })
+  email: string;
+
   @ApiProperty({
     description: '유저 닉네임',
     example: '라이빗',
@@ -15,5 +35,6 @@ export class SignupDto {
     description: '마케팅 동의 여부',
     example: true,
   })
+  @IsBoolean()
   marketingConsent: boolean;
 }
