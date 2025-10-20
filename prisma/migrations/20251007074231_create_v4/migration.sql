c5704eb1e062662a01150271ea5867bf3d2a2283
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-10-20 22:07:19 +0900

    :bug: fix: 마이그레이션 에러 해결

--- prisma/migrations/20251007074231_create_v4/migration.sql
+++ prisma/migrations/20251007074231_create_v4/migration.sql
@@ -9,20 +9,20 @@
   - A unique constraint covering the columns `[title,artist]` on the table `songs` will be added. If there are existing duplicate values, this will fail.
 
 */
--- CreateIndex
-CREATE UNIQUE INDEX `uk_artist` ON `artists`(`artist`);
+-- -- CreateIndex
+-- CREATE UNIQUE INDEX `uk_artist` ON `artists`(`artist`);
 
--- CreateIndex
-CREATE UNIQUE INDEX `uk_concert_category` ON `concert_info`(`concert_id`, `category`);
+-- -- CreateIndex
+-- CREATE UNIQUE INDEX `uk_concert_category` ON `concert_info`(`concert_id`, `category`);
 
--- CreateIndex
-CREATE UNIQUE INDEX `uk_title` ON `concerts`(`title`);
+-- -- CreateIndex
+-- CREATE UNIQUE INDEX `uk_title` ON `concerts`(`title`);
 
--- CreateIndex
-CREATE UNIQUE INDEX `uk_concert_title` ON `cultures`(`concert_id`, `title`(255));
+-- -- CreateIndex
+-- CREATE UNIQUE INDEX `uk_concert_title` ON `cultures`(`concert_id`, `title`(255));
 
--- CreateIndex
-CREATE UNIQUE INDEX `uk_title_artist` ON `setlists`(`title`, `artist`);
+-- -- CreateIndex
+-- CREATE UNIQUE INDEX `uk_title_artist` ON `setlists`(`title`, `artist`);
 
--- CreateIndex
-CREATE UNIQUE INDEX `uk_title_artist` ON `songs`(`title`, `artist`);
+-- -- CreateIndex
+-- CREATE UNIQUE INDEX `uk_title_artist` ON `songs`(`title`, `artist`);
