11c8808749b9eb46ca24452b6e04dbcb005c01ff
JEEWOOCHOI <jeewoochoi47@gmail.com>
2025-12-29 21:30:11 +0900

    :bug: fix: 애플 로그인 및 리프레시 토큰 발급 DTO에 validator 추가

--- src/v4/auth/dto/refresh-token.dto.ts
+++ src/v4/auth/dto/refresh-token.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
 export class RefreshTokenDto {
   @ApiProperty({
     description: '리프레시 토큰',
     example: 'YOUR_REFRESH_TOKEN_HERE',
   })
+  @IsString()
+  @IsOptional()
   refreshToken?: string;
 }
