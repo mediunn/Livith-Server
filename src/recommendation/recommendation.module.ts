893f05bfc29b600beb7e7da5cced824ec9004f28
junhui <junhuilim@kakao.com>
2026-01-17 12:32:58 +0900

    ✨feat: 추천 기능 구현

--- src/recommendation/recommendation.module.ts
+++ src/recommendation/recommendation.module.ts
@@ -3,12 +3,16 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { LastfmApiService } from "./services/last-fm.api.service";
 import { MusicApiFactoryService } from "./services/music-api-factory.service";
+import { PrismaModule } from "prisma/prisma.module";
+import { RecommendationController } from "./recommendation.controller";
+import { RecommendationService } from "./services/recommendation.service";
 
 
 
 @Module({
-    imports: [HttpModule, ConfigModule],
-    providers: [LastfmApiService, MusicApiFactoryService],
+    imports: [HttpModule, ConfigModule, PrismaModule],
+    controllers: [RecommendationController],
+    providers: [LastfmApiService, MusicApiFactoryService, RecommendationService],
     exports: [MusicApiFactoryService, LastfmApiService],
 })
 export class RecommendationModule{}
