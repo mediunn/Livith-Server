3368094f7f82918c3ea21274cc05e97560ebb89d
junhui <junhuilim@kakao.com>
2026-01-21 17:18:34 +0900

    ♻️ refactor: 페이지네이션 삭제 및 integration 구조로 통합

--- src/recommendation/recommendation.module.ts
+++ src/recommendation/recommendation.module.ts
@@ -1,18 +1,22 @@
 import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { LastfmApiService } from "./services/last-fm.api.service";
+import { LastfmApiService } from "./integrations/lastfm/last-fm.api.service";
 import { MusicApiFactoryService } from "./services/music-api-factory.service";
 import { PrismaModule } from "prisma/prisma.module";
 import { RecommendationController } from "./recommendation.controller";
 import { RecommendationService } from "./services/recommendation.service";
+import { YoutubeApiService } from "./integrations/youtube/youtube.api.service";
+import { ArtistImageService } from "./services/artist-image.service";
+import { ArtistSyncService } from "./services/artist-sync.service";
+import { GlobalArtistCacheService } from "./services/global-artist-cache.service";
 
 
 
 @Module({
     imports: [HttpModule, ConfigModule, PrismaModule],
     controllers: [RecommendationController],
-    providers: [LastfmApiService, MusicApiFactoryService, RecommendationService],
-    exports: [MusicApiFactoryService, LastfmApiService],
+    providers: [LastfmApiService, MusicApiFactoryService, YoutubeApiService, ArtistImageService, ArtistSyncService, RecommendationService, GlobalArtistCacheService],
+    exports: [MusicApiFactoryService, LastfmApiService, GlobalArtistCacheService],
 })
 export class RecommendationModule{}
