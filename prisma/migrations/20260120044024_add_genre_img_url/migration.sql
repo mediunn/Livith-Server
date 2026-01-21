083673ca8bfa88e3a8b3df099bf8ef25f4d789d1
junhui <junhuilim@kakao.com>
2026-01-21 22:31:27 +0900

    🔧fix: 마이그레이션 중복 수정

--- prisma/migrations/20260120044024_add_genre_img_url/migration.sql
+++ prisma/migrations/20260120044024_add_genre_img_url/migration.sql
@@ -5,11 +5,11 @@
 
 */
 -- AlterTable
-ALTER TABLE `genres` ADD COLUMN `img_url` VARCHAR(2048) NOT NULL;
+/*ALTER TABLE `genres` ADD COLUMN `img_url` VARCHAR(2048) NOT NULL;
 
 UPDATE genres SET img_url = 'https://i.imgur.com/YMlRF0K.jpeg' WHERE name = 'ACOUSTIC';
 UPDATE genres SET img_url = 'https://i.imgur.com/Odi5v7K.jpeg' WHERE name = 'JPOP';
 UPDATE genres SET img_url = 'https://i.imgur.com/0rnKZo2.jpeg' WHERE name = 'RAP_HIPHOP';
 UPDATE genres SET img_url = 'https://i.imgur.com/UBxSeqS.jpeg' WHERE name = 'ELECTRONIC';
 UPDATE genres SET img_url = 'https://i.imgur.com/uCkAExC.jpeg' WHERE name = 'ROCK_METAL';
-UPDATE genres SET img_url = 'https://i.imgur.com/DsmIq5O.jpeg' WHERE name = 'CLASSIC_JAZZ';
+UPDATE genres SET img_url = 'https://i.imgur.com/DsmIq5O.jpeg' WHERE name = 'CLASSIC_JAZZ';*/
