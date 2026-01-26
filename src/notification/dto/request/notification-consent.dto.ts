7f09c2bfae0b381ee8c6c629f9547134c4d66462
junhui <junhuilim@kakao.com>
2026-01-26 16:02:52 +0900

    ✨feat: 알림 동의 요청 DTO 추가

---
+++ src/notification/dto/request/notification-consent.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean } from 'class-validator';
+
+
+export class NotificationConsentDto{
+    @ApiProperty({
+        description: '광고성 정보 수신 동의',
+        example: true,
+    })
+    @IsBoolean()
+    marktetingConsent: boolean;
+
+    @ApiProperty({
+        description: '야간 푸시 알림 동의',
+        example: true,
+    })
+    @IsBoolean()
+    nightAlertConsent: boolean;
+}
