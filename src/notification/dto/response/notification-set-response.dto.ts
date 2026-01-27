376bcd8f7fa02638b9d3a03000ece07ff5fcfc0c
junhui <junhuilim@kakao.com>
2026-01-27 16:38:22 +0900

    ♻️ refactor: 알림 dto 리팩토링

--- src/notification/dto/response/notification-set-response.dto.ts
+++ src/notification/dto/response/notification-set-response.dto.ts
@@ -1,58 +1,32 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { NotificationSet } from '@prisma/client';
 
+// 알림 설정 조회 응답 DTO
+export class NotificationSettingResponseDto {
+  @ApiProperty({ description: '유저를 위한 혜택 알림', example: false })
+  benefitAlert: boolean;
 
-export class NotificationSettingResponseDto{
-    @ApiProperty({
-        description: '예매 일정 알림',
-        example: true,
-    })
-    ticketAlert: boolean;
+  @ApiProperty({ description: '야간 알림 허용 (21시~08시)', example: false })
+  nightAlert: boolean;
 
-    @ApiProperty({
-        description: '콘서트 정보 업데이트 알림',
-        example: true,
-    })
-    infoAlert: boolean;
+  @ApiProperty({ description: '예매 일정 알림', example: true })
+  ticketAlert: boolean;
 
-    @ApiProperty({
-        description: '좋아하는 아티스트 콘서트 오픈 알림',
-        example: true,
-    })
-    interestAlert: boolean;
+  @ApiProperty({ description: '콘서트 정보 업데이트 알림', example: true })
+  infoAlert: boolean;
 
-    @ApiProperty({
-        description: '취향 기반 콘서트 알림 (홍보성)',
-        example: false,
-    })
-    recommendAlert: boolean;
+  @ApiProperty({ description: '좋아하는 아티스트 콘서트 오픈 알림', example: true })
+  interestAlert: boolean;
 
-    @ApiProperty({
-        description: '야간 알림 허용 (21시~08시)',
-        example: false,
-    })
-    nightAlert: boolean;
+  @ApiProperty({ description: '취향 기반 콘서트 알림', example: true })
+  recommendAlert: boolean;
 
-    @ApiProperty({
-        description: '홍보성 알림 동의 여부(배너 표시용)',
-        example: false,
-    })
-    hasMarketingConsent: boolean;
-
-    constructor(
-        notificationSet: {
-            ticketAlert: boolean;
-            infoAlert: boolean;
-            interestAlert: boolean;
-            recommendAlert: boolean;
-            nightAlert: boolean;
-        },
-        hasMarketingConsent: boolean,
-    ){
-        this.ticketAlert = notificationSet.ticketAlert;
-        this.infoAlert = notificationSet.infoAlert;
-        this.interestAlert = notificationSet.interestAlert;
-        this.recommendAlert = notificationSet.recommendAlert;
-        this.nightAlert = notificationSet.nightAlert;
-        this.hasMarketingConsent = hasMarketingConsent;
-    }
+  constructor(notificationSet: NotificationSet) {
+    this.benefitAlert = notificationSet.benefitAlert;
+    this.nightAlert = notificationSet.nightAlert;
+    this.ticketAlert = notificationSet.ticketAlert;
+    this.infoAlert = notificationSet.infoAlert;
+    this.interestAlert = notificationSet.interestAlert;
+    this.recommendAlert = notificationSet.recommendAlert;
+  }
 }
