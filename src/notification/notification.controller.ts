d28db7eca50d615571d4b5605061dcad1afc5689
junhui <junhuilim@kakao.com>
2026-01-28 16:32:26 +0900

    ✨feat: 알림 dto 및 api 구현

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { API_PREFIX } from 'src/common/constants/api-prefix';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { NotificationService } from './notification.service';
 import { CreateNotificationConsentDto } from './dto/request/create-notification-consent.dto';
 import { CurrentUser } from '../common/decorator/current-user.decorator';
+import { GetNotificationDto } from './dto/request/get-notification.dto';
+import { NotificationResponseDto } from './dto/response/notification-response.dto';
 
 
 
@@ -45,4 +47,42 @@ export class NotificationController {
   ) {
     return this.notificationService.createNotificationConsent(user.userId, dto.field, dto.isAgreed);
   }
+
+  @Get()
+  @ApiOperation({summary: '알림 목록 조회'})
+  async getNotifications(
+    @CurrentUser() user: JwtPayload,
+    @Query() dto: GetNotificationDto,
+  ): Promise<NotificationResponseDto[]>{
+    return this.notificationService.getNotifications(user.userId, dto.cursor, dto.size);
+  }
+
+  @Get('unread-count')
+  @ApiOperation({summary: '읽지 않는 알림 개수 조회'})
+  async getUnreadCount(
+    @CurrentUser() user: JwtPayload
+  ): Promise<{unreadCount: number}>{
+    const unreadCount = await this.notificationService.getUnreadCount(user.userId);
+    return {unreadCount};
+  }
+
+  @Patch(':id/read')
+  @ApiOperation({summary: '알림 읽음 처리'})
+  async markAsRead(
+    @CurrentUser() user: JwtPayload,
+    @Param('id', ParseIntPipe) notificationId: number,
+  ): Promise<{success: boolean, message: string}>{
+    await this.notificationService.markAsRead(user.userId, notificationId);
+    return {success: true, message: '알림을 읽음 처리했습니다.'};
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '알림 삭제' })
+  async deleteNotification(
+    @CurrentUser() user: JwtPayload,
+    @Param('id', ParseIntPipe) notificationId: number,
+  ): Promise<{ success: boolean; message: string }> {
+    await this.notificationService.deleteNotification(user.userId, notificationId);
+    return { success: true, message: '알림을 삭제했습니다.' };
+  } 
 }
