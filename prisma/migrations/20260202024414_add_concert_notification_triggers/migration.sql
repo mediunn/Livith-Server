b4c3318b7ed84b6a5c9304616253fbdb44d2d8db
junhui <junhuilim@kakao.com>
2026-02-02 18:04:44 +0900

    ✨feat: mysql용 클라이언트 구문 삭제(delimiter)

--- prisma/migrations/20260202024414_add_concert_notification_triggers/migration.sql
+++ prisma/migrations/20260202024414_add_concert_notification_triggers/migration.sql
@@ -1,16 +1,13 @@
-// concert INSERT 시: 아티스트 콘서트 오픈 알림 큐에 적재
-DELIMITER //
+-- concert INSERT 시: 아티스트 콘서트 오픈 알림 큐에 적재
 CREATE TRIGGER after_concert_insert
 AFTER INSERT ON concerts
 FOR EACH ROW
 BEGIN
   INSERT INTO concert_notification_queue (concert_id, event_type, processed)
   VALUES (NEW.id, 'ARTIST_CONCERT_OPEN', 0);
-END//
-DELIMITER ;
+END;
 
 -- concerts UPDATE 시: 콘서트 상세 정보 업데이트 알림 큐에 적재 (티켓 정보 제외)
-DELIMITER //
 CREATE TRIGGER after_concert_update
 AFTER UPDATE ON concerts
 FOR EACH ROW
@@ -19,5 +16,5 @@ BEGIN
     INSERT INTO concert_notification_queue (concert_id, event_type, processed)
     VALUES (NEW.id, 'CONCERT_INFO_UPDATE_CONCERT_DETAIL', 0);
   END IF;
-END//
-DELIMITER ;
+END;
+
