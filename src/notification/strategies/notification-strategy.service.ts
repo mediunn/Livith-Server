b9f80ae1ca221b9a0329a85df26fe6578f5d0464
junhui <junhuilim@kakao.com>
2026-02-07 01:13:42 +0900

    ✨feat: 누락된 알림 추가(interest-concert)

--- src/notification/strategies/notification-strategy.service.ts
+++ src/notification/strategies/notification-strategy.service.ts
@@ -3,6 +3,7 @@ import { NotificationType } from '@prisma/client';
 import { NotificationStrategy } from './notification-strategy.interface';
 import { ArtistConcertOpenStrategy } from './artist-concert-open.strategy';
 import { ConcertInfoUpdateStrategy } from './concert-info-update.strategy';
+import { InterestConcertStrategy } from './interest-concert.strategy';
 import { RecommendationStrategy } from './recommendation.strategy';
 import { TicketReminderStrategy } from './ticket-reminder.strategy';
 import { PrismaService } from 'prisma/prisma.service';
@@ -15,11 +16,13 @@ export class NotificationStrategyService {
     private readonly prisma: PrismaService,
     private readonly artistConcertOpenStrategy: ArtistConcertOpenStrategy,
     private readonly concertInfoUpdateStrategy: ConcertInfoUpdateStrategy,
+    private readonly interestConcertStrategy: InterestConcertStrategy,
     private readonly recommendationStrategy: RecommendationStrategy,
   ) {
     this.strategies = new Map<NotificationType, NotificationStrategy>([
       [NotificationType.ARTIST_CONCERT_OPEN, artistConcertOpenStrategy],
       [NotificationType.CONCERT_INFO_UPDATE, concertInfoUpdateStrategy],
+      [NotificationType.INTEREST_CONCERT, interestConcertStrategy],
       [NotificationType.RECOMMEND, recommendationStrategy],
     ]);
   }
