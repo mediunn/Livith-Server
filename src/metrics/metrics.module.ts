53d90dda33819fc4b295807cae76f846c02a046b
junhui <junhuilim@kakao.com>
2026-02-07 02:17:54 +0900

    🔧chore: metric 모듈 활성화 및 엔드포인트 구현

---
+++ src/metrics/metrics.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { PrometheusModule } from "@willsoto/nestjs-prometheus";
+
+
+@Module({
+    imports: [
+        PrometheusModule.register({
+            path: '/metrics', // 매트릭 엔드포인트
+            defaultMetrics: {
+                enabled: true, // Node.js 기본 메트릭 활성화
+            },
+        }),
+    ],
+    exports: [PrometheusModule],
+})
+export class MetricsModule{}
