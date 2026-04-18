a253be87142cdcb8a59f033df5a7a87c27a7fbe7
junhui <junhuilim@kakao.com>
2026-04-18 18:02:52 +0900

    ♻️ refactor: GenreType 신규 값에 맞춰 enum/매퍼/테스트 정리

--- src/recommendation/integrations/spotify/genre-mapping.util.ts
+++ src/recommendation/integrations/spotify/genre-mapping.util.ts
@@ -2,9 +2,8 @@ export const SPOTIFY_GENRE_TAG_MAPPING: Record<string, string> = {
   JPOP: 'j-pop',
   ROCK_METAL: 'rock',
   RAP_HIPHOP: 'hip-hop',
-  CLASSIC_JAZZ: 'jazz',
-  ACOUSTIC: 'acoustic',
-  ELECTRONIC: 'electronic',
+  POP: 'pop',
+  INDIE: 'indie',
 };
 
 export function getSpotifyGenreTag(genreType: string): string {
