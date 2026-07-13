81570a3252533d63fe1cbac47214b69530cad360
junhui <junhuilim@kakao.com>
2026-07-13 22:01:59 +0900

    ✨feat: 추출 에러코드 및 에러메시지 추가

--- src/common/constants/error-messages.ts
+++ src/common/constants/error-messages.ts
@@ -14,6 +14,7 @@ export const ErrorMessages: Record<ErrorCode, string> = {
     '해당 셋리스트와 콘서트의 조합이 존재하지 않습니다.',
   [ErrorCode.GENRE_NOT_FOUND]: '해당 장르를 찾을 수 없습니다.',
   [ErrorCode.NOTIFICATION_NOT_FOUND]: '해당 알림을 찾을 수 없습니다.',
+  [ErrorCode.EXTRACTION_JOB_NOT_FOUND]: '추출 작업을 찾을 수 없습니다.',
 
   // Bad Request
   [ErrorCode.INVALID_CURSOR_FORMAT]: '유효하지 않은 cursor 형식입니다.',
@@ -22,6 +23,7 @@ export const ErrorMessages: Record<ErrorCode, string> = {
   [ErrorCode.USER_ALREADY_DELETED]: '이미 탈퇴한 회원입니다.',
   [ErrorCode.NICKNAME_ALREADY_EXISTS]: '이미 존재하는 닉네임이에요.',
   [ErrorCode.USER_ALREADY_EXISTS]: '이미 존재하는 유저입니다.',
+  [ErrorCode.INVALID_INSTAGRAM_URL]: '유효한 인스타그램 게시글 URL이 아닙니다.',
 
   // Forbidden
   [ErrorCode.USER_DELETED]: '탈퇴한 회원입니다.',
