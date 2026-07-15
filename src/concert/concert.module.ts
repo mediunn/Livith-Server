465d3c79a0513399bfd1212e0972f466c0c5d4e5
JEEWOOCHOI <jeewoochoi47@gmail.com>
2026-07-15 16:17:34 +0900

    :sparkles: feat: 콘서트 정보 요청 API 구현

--- src/concert/concert.module.ts
+++ src/concert/concert.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConcertService } from './concert.service';
 import { ConcertController } from './concert.controller';
 import { PrismaModule } from 'prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   providers: [ConcertService],
   controllers: [ConcertController],
 })
